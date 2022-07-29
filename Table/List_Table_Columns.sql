2c80b04af063eb74cb06be69692cf55b91a7c19c
Maxim Korsukov <mkorsukov@gmail.com>
2022-07-29 15:34:51 -0500

    Update script

--- Table/List_Table_Columns.sql
+++ Table/List_Table_Columns.sql
@@ -7,9 +7,10 @@ select
     when 'varchar' then DATA_TYPE + '(' + cast(CHARACTER_MAXIMUM_LENGTH as varchar) + ')'
     when 'decimal' then DATA_TYPE + '(' + cast(NUMERIC_PRECISION as varchar) + ',' + cast(NUMERIC_SCALE as varchar) + ')'
     else DATA_TYPE
-  end as "DATA_TYPE"
+  end as "DATA_TYPE",
+  IS_NULLABLE
 from INFORMATION_SCHEMA.COLUMNS
 where
-  TABLE_NAME = 'ProfitEstimate'
+  TABLE_NAME = 'Table'
 order by
   ORDINAL_POSITION;
