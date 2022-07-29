22870332eb45f6d1e202be73adf42ae4414bc24f
Maxim Korsukov <mkorsukov@gmail.com>
2022-07-29 11:32:05 -0500

    Add script

---
+++ Table/List_Table_Columns.sql
@@ -0,0 +1,15 @@
+set nocount on;
+
+select
+  COLUMN_NAME,
+  case DATA_TYPE
+    when 'nvarchar' then DATA_TYPE + '(' + cast(CHARACTER_MAXIMUM_LENGTH as varchar) + ')'
+    when 'varchar' then DATA_TYPE + '(' + cast(CHARACTER_MAXIMUM_LENGTH as varchar) + ')'
+    when 'decimal' then DATA_TYPE + '(' + cast(NUMERIC_PRECISION as varchar) + ',' + cast(NUMERIC_SCALE as varchar) + ')'
+    else DATA_TYPE
+  end as "DATA_TYPE"
+from INFORMATION_SCHEMA.COLUMNS
+where
+  TABLE_NAME = 'ProfitEstimate'
+order by
+  ORDINAL_POSITION;
