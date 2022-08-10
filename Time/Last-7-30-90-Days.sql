4f2c4e38a2e0c5cb3e2102514789e669934497d6
Maxim Korsukov <mkorsukov@gmail.com>
2022-08-10 13:56:54 -0500

    Optimize script

--- Time/Last-7-30-90-Days.sql
+++ Time/Last-7-30-90-Days.sql
@@ -3,14 +3,10 @@ declare @TimeOffset int = -300;
 set nocount on;
 set datefirst 1;
 
-declare @NowTime datetimeoffset(0) = switchoffset(getutcdate(), @TimeOffset);
-declare @Last7Days date = dateadd(dd, -7, @NowTime);
-declare @Last30Days date = dateadd(dd, -30, @NowTime);
-declare @Last90Days date = dateadd(dd, -90, @NowTime);
-
-declare @Last7DaysStartTime datetimeoffset(0) = todatetimeoffset(datetimefromparts(datepart(yy, @Last7Days), datepart(mm, @Last7Days), datepart(dd, @Last7Days), 0, 0, 0, 0), @TimeOffset);
-declare @Last30DaysStartTime datetimeoffset(0) = todatetimeoffset(datetimefromparts(datepart(yy, @Last30Days), datepart(mm, @Last30Days), datepart(dd, @Last30Days), 0, 0, 0, 0), @TimeOffset);
-declare @Last90DaysStartTime datetimeoffset(0) = todatetimeoffset(datetimefromparts(datepart(yy, @Last90Days), datepart(mm, @Last90Days), datepart(dd, @Last90Days), 0, 0, 0, 0), @TimeOffset);
+declare @NowTime date = switchoffset(getutcdate(), @TimeOffset);
+declare @Last7DaysStartTime datetimeoffset(0) = todatetimeoffset(dateadd(dd, -7, @NowTime), @TimeOffset);
+declare @Last30DaysStartTime datetimeoffset(0) = todatetimeoffset(dateadd(dd, -30, @NowTime), @TimeOffset);
+declare @Last90DaysStartTime datetimeoffset(0) = todatetimeoffset(dateadd(dd, -90, @NowTime), @TimeOffset);
 
 print @Last7DaysStartTime;
 print @Last30DaysStartTime;
